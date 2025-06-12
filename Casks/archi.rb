45e5a8c268ff41d7f1bfc9999153a6e1a0e3ed05
PH Schott <71520751+phschott@users.noreply.github.com>
2025-06-12 16:07:41 +0200

    Update archi.rb

--- Casks/archi.rb
+++ Casks/archi.rb
@@ -2,7 +2,7 @@ cask "archi" do
   arch arm: "Silicon-"
 
   version "5.5.0"
-  sha256 arm:   "83914582fef0e07b36e869545c2a55dd542336b59ddf9e6fb71b7a143f380d68",
+  sha256 arm:    "83914582fef0e07b36e869545c2a55dd542336b59ddf9e6fb71b7a143f380d68",
          x86_64: "05e40f4ec638a600aca04e0ff9d30a13e3f750ffbc5a25136e25857aca64cec9"
 
   url "https://www.archimatetool.com/downloads/archi/#{version.major_minor}/Archi-Mac-#{arch}#{version}.dmg"
