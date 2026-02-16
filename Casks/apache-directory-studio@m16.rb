8ff2b7c817a92f177187eeb977692af6f392d37a
Pierre-Henri Schott <ph@schott.io>
2026-02-16 15:53:30 +0100

    Testing

--- Casks/apache-directory-studio@m16.rb
+++ Casks/apache-directory-studio@m16.rb
@@ -2,7 +2,8 @@ cask "apache-directory-studio@m16" do
   version "2.0.0.v20210213-M16"
   sha256 "aeb174447146c5a7b53158047f566ce63a93fbeef06e2efb4c9567930210bf2e"
 
-  url "https://www.apache.org/dyn/closer.lua?path=directory/studio/#{version}/ApacheDirectoryStudio-#{version}-macosx.cocoa.x86_64.dmg?action=download"
+  url "https://archive.apache.org/dist/directory/studio/#{version}/ApacheDirectoryStudio-#{version}-macosx.cocoa.x86_64.dmg",
+      referer: "https://archive.apache.org/"
   name "Apache Directory Studio"
   desc "Eclipse-based LDAP browser and directory client"
   homepage "https://directory.apache.org/studio/"
