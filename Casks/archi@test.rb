5cc0a2093fd147478870779587d59841789f81b7
SCHOTT Pierre-Henri <pierrehenri.schott@axa.com>
2026-03-23 18:01:12 +0100

    Fix sha spacing

--- Casks/archi@test.rb
+++ Casks/archi@test.rb
@@ -35,8 +35,8 @@ cask "archi@test" do
   arch arm: "-Silicon"
 
   version "5.8.0"
-  sha256 arm:          "0add079afc3aae592cfff44bd9af0e9bffd0dcddad783a68c58721e0878015bc",
-         intel:        "ab6a18c6d4b5d39ee368d307e1e15bf8db485a6ea68e54b69c2ffeffc2a2d7f1"
+  sha256 arm:   "0add079afc3aae592cfff44bd9af0e9bffd0dcddad783a68c58721e0878015bc",
+         intel: "ab6a18c6d4b5d39ee368d307e1e15bf8db485a6ea68e54b69c2ffeffc2a2d7f1"
 
   url "https://github.com/archimatetool/archi.io/releases/download/#{version}/Archi-Mac#{arch}-#{version}.dmg",
       verified: "github.com/archimatetool/archi.io/"
