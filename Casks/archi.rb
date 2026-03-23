7d096cdd1512cdefc604ca120f18b0a86593d4dc
SCHOTT Pierre-Henri <pierrehenri.schott@axa.com>
2026-03-23 17:38:34 +0100

    Fix URL

--- Casks/archi.rb
+++ Casks/archi.rb
@@ -5,7 +5,8 @@ cask "archi" do
   sha256 arm:          "0add079afc3aae592cfff44bd9af0e9bffd0dcddad783a68c58721e0878015bc",
          intel:        "ab6a18c6d4b5d39ee368d307e1e15bf8db485a6ea68e54b69c2ffeffc2a2d7f1"
 
-  url "https://www.archimatetool.com/downloads/archi/#{version}/Archi-Mac#{arch}-#{version}.dmg"
+  url "https://github.com/archimatetool/archi.io/releases/download/#{version}/Archi-Mac#{arch}-#{version}.dmg",
+      verified: "github.com/archimatetool/archi.io/"
   name "Archimate modeling"
   desc "Toolkit for the ArchiMate modelling language"
   homepage "https://www.archimatetool.com/"
